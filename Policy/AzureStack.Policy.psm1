b57f5a8630fefdcc7e887c799adca2a798678ac9
igorse <igorse@microsoft.com>
2016-08-26 12:46:11 -0700

    Initial upload
    Includes Azure Stack ARM policy tool

---
+++ Policy/AzureStack.Policy.psm1
@@ -0,0 +1,74 @@
+function Get-AzureStackRmPolicy 
+{
+    $defaults = [System.IO.Path]::GetDirectoryName($PSCommandPath)
+
+    $providerMetadata = ConvertFrom-Json (Get-Content -Path ($defaults + "\AzureStack.Provider.Metadata.json") -Raw)
+    $vmSkus = @() + (ConvertFrom-Json (Get-Content -Path ($defaults + "\AzureStack.vmSkus.json") -Raw))
+    $storageSkus = @() + (ConvertFrom-Json (Get-Content -Path ($defaults + "\AzureStack.storageSkus.json") -Raw))
+    
+    $allowResources = @()
+
+    foreach ($p in $providerMetadata.value) 
+    {
+        foreach ($r in $p.resourceTypes)
+        {
+            $allowResources += @{ field = "type"; equals = $p.namespace + "/" + $r.ResourceType }
+        }
+    }
+
+    $vmSkuField = "Microsoft.Compute/virtualMachines/sku.name"
+    $storageSkuField = "Microsoft.Storage/storageAccounts/sku.name"
+
+    $policy = @{
+        if = @{
+            not = @{
+                allOf = @(
+                    @{
+                        anyOf = $allowResources
+                    },
+                    @{
+                        not = @{
+                            anyOf = @(
+                                @{
+                                    allOf = @(
+                                        @{
+                                            field = $vmSkuField;
+                                            exists = "true"
+                                        },
+                                        @{
+                                            not = @{
+                                                field = $vmSkuField;
+                                                in = $vmSkus
+                                            }
+                                        }
+                                    )
+                                },
+                                @{
+                                    allOf = @(
+                                        @{
+                                            field = $storageSkuField;
+                                            exists = "true"
+                                        },
+                                        @{
+                                            not = @{
+                                                field = $storageSkuField;
+                                                in = $vmSkus
+                                            }
+                                        }
+                                    )
+                                }
+                            )
+                        }
+                    }
+                )
+            }
+        };
+        then = @{
+            effect = "deny"
+        }
+    }
+
+    ConvertTo-Json $policy -Depth 100
+}
+
+Export-ModuleMember Get-AzureStackRmPolicy
---
+++ README.md
@@ -0,0 +1,1 @@
+# Tools for using Azure and Azure Stack
