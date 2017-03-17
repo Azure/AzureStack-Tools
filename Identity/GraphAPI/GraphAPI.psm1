938964c4604260e4f0b8cef57b4ee46e9f3d3249
shnatara <shnatara@SHRIRAM-DESK2>
2017-03-17 15:04:23 -0700

    Integrating MT changes to Identity Module

--- Identity/GraphAPI/GraphAPI.psm1
+++ Identity/GraphAPI/GraphAPI.psm1
@@ -1,8 +1,5 @@
-﻿<###################################################
- #                                                 #
- #  Copyright (c) Microsoft. All rights reserved.  #
- #                                                 #
- ##################################################>
+﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# See LICENSE.txt in the project root for license information.
 
 <#
 .Synopsis
