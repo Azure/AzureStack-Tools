df7498dcf5d37f76cf84b2c5251ef8384a68ba9e
GeorgeH-MS <30331846+GeorgeH-MS@users.noreply.github.com>
2020-09-08 13:53:28 +0800

    Add files via upload

---
+++ CapacityManagement/AzureStack.CapacityMgmt.psm1
@@ -0,0 +1,199 @@
+﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# See LICENSE.txt in the project root for license information.
+
+#Prepare volume path
+$ScaleUnit = (Get-AzsScaleUnit)[0]
+$StorageSubSystem = (Get-AzsStorageSubSystem -ScaleUnit $ScaleUnit.Name)[0]
+$SubSystemName = $StorageSubSystem.Name
+$LocationPath = $SubSystemName.Split("/")[2]
+$LocationPath = $LocationPath.Substring($LocationPath.IndexOf(".")+1)
+$LocationPath = "\\SU1FileServer."+$LocationPath+"\SU1_"
+
+
+<#
+    .SYNOPSIS
+    Query the volume with least available capacity
+#>
+
+function GetMigrationSource {
+
+    $Volumes = (Get-AzsVolume -ScaleUnit $ScaleUnit.Name -StorageSubSystem $StorageSubSystem.Name | Where-Object {$_.VolumeLabel -Like "ObjStore_*"})
+    $Volume  = ($Volumes | Sort-Object RemainingCapacityGB)[0]
+    return $Volume.VolumeLabel
+}
+
+<#
+    .SYNOPSIS
+    Prepare the volume need to be managed for further analytic
+#>
+
+function PrepareMigrationSource {
+    param (
+        [parameter(Mandatory = $false, HelpMessage = "Volume which need to be free space, If this parameter isn't specified, the volume with least available capacity would be selected by default")]
+        [string] $VolumeLabel
+    )
+
+    if ($VolumeLabel) {
+        if ($VolumeLabel -cnotmatch '^ObjStore_([1-9]|1[0-6])$') {
+            throw "ERROR: Source Volume should follow the expression pattern 'ObjStore_X', x is a number ranged from 1 to 16"
+            return
+        }
+        else {
+            $MigrationSource = $LocationPath+$VolumeLabel
+        }
+    } else {
+        $VolumeLabel = GetMigrationSource
+        $MigrationSource = $LocationPath+$VolumeLabel
+    }
+    return $MigrationSource
+}
+
+<#
+    .SYNOPSIS
+    Get unattached managed disks and export to CSVs
+#>
+
+function GetUnattchedDisks {
+    param (
+        [parameter(Mandatory = $false, HelpMessage = "Volume which need to be free space, If this parameter isn't specified, the volume with least available capacity would be selected by default")]
+        [string] $VolumeLabel,
+
+        [parameter(Mandatory = $false, HelpMessage = "Group the unattached disks by user subscription")]
+        [switch]$GroupBySubscription,
+
+        [parameter(Mandatory = $false, HelpMessage = "Export the unattached disk list to CSVs, each user subscription would be exported to a separate CSV file")]
+        [switch]$ExportToCSV,
+
+        [parameter(Mandatory = $false, HelpMessage = "File path to export the unattached disk list. If not specified, all CSVs would be exported to 'UnattachedDisks' folder under current location")]
+        [string]$ExportFolder
+    )
+    
+    $MigrationSource = PrepareMigrationSource -VolumeLabel $VolumeLabel
+    $UnattachedDisks = Get-AzsDisk -Status Recommended -SharePath $MigrationSource -Count 1000 | where {$_.DiskType -eq 'Disk'} | select `
+      Status, 
+      @{Name="DiskSubscription";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[1] }}, `
+      @{Name="DiskResourceGroup";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[2] }}, `
+      @{Name="DiskName";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[3] }}, `
+      ProvisionSizeGB, `
+      ActualSizeGB
+    if ($GroupBySubscription) {
+        $UnattachedDisks = $UnattachedDisks | Group-Object -Property DiskSubscription | select  @{n="Subscription";e={$_.name}}, @{n="DiskCount";e={$_.count}}, @{n="TotalSize";e={($_.Group | Measure-Object -Property ActualSizeGB -Sum).Sum}} | sort TotalSize -Descending
+    }
+    if ($ExportToCSV) {
+        if (!($GroupBySubscription)) {
+            throw "ERROR: Export to CSV file only works when 'GroupBySubscription' option is turned on"
+        }
+        if ($ExportFolder) {
+            $folderName = $ExportFolder+"\UnattachedDisks"
+        } else {
+            $folderName = "UnattachedDisks"
+        }
+        if (!(Test-Path -Path $folderName))
+        {
+            New-Item ($folderName) -ItemType Directory | Out-Null
+        }
+        foreach ($Sub in $UnattachedDisks) {
+            $FileName = $folderName+"\Unattached_("+$Sub.DiskCount+")"+$Sub.Subscription+".csv"
+            Get-AzsDisk -Status Recommended -SharePath $MigrationSource -Count 1000 -UserSubscriptionId $Sub.Subscription | where {$_.DiskType -eq 'Disk'} | Select `
+                Status, 
+                @{Name="DiskSubscription";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[1] }}, `
+                @{Name="DiskResourceGroup";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[2] }}, `
+                @{Name="DiskName";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[3] }}, `
+                UserResourceId,
+                ActualSizeGb,
+                ProvisionSizeGB | Export-Csv -Path $FileName
+        }
+    }
+    return $UnattachedDisks
+}
+
+<#
+    .SYNOPSIS
+    Get attached managed disks and export to CSVs
+#>
+
+function GetAttchedDisks {
+    param (
+        [parameter(Mandatory = $false, HelpMessage = "Volume which need to be free space, If this parameter isn't specified, the volume with least available capacity would be selected by default")]
+        [string] $VolumeLabel,
+
+        [parameter(Mandatory = $false, HelpMessage = "Group the unattached disks by user subscription")]
+        [switch]$GroupBySubscription,
+
+        [parameter(Mandatory = $false, HelpMessage = "Export the unattached disk list to CSVs, each user subscription would be exported to a separate CSV file")]
+        [switch]$ExportToCSV,
+
+        [parameter(Mandatory = $false, HelpMessage = "File path to export the unattached disk list. If not specified, all CSVs would be exported to 'AttachedDisks' folder under current location")]
+        [string]$ExportFolder
+    )
+    
+    $MigrationSource = PrepareMigrationSource -VolumeLabel $VolumeLabel
+    $AttachedDisks = Get-AzsDisk -status all -SharePath $MigrationSource -Count 1000 | where {$_.status -in @("OnlineMigration","Attached","Reserved")} | select `
+      Status, 
+      DiskType, 
+      @{Name="Volume";Expression={ $m = $_.SharePath -match "\\\\.*\\(.*)"; $matches[1] }}, `
+      @{Name="OwnerSubscription";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)"; $matches[1] }}, `
+      @{Name="OwnerResourceGroup";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)"; $matches[2] }}, `
+      @{Name="OwnerName";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)"; $matches[3] }}, `
+      @{Name="DiskSnapshotName";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[3] }}, `
+      @{Name="DiskSnapshotResourceGroup";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[2] }}, `
+      ProvisionSizeGB, `
+      ActualSizeGB, `
+      DiskSku
+    if ($GroupBySubscription) {
+        $AttachedDisks = $AttachedDisks | Group-Object -Property OwnerSubscription | select @{n="DiskCount";e={$_.count}}, @{n="TotalSize";e={($_.Group | Measure-Object -Property ActualSizeGB -Sum).Sum}}, @{n="OwnerSubscription";e={$_.name}} | sort TotalSize -Descending
+    }
+    if ($ExportToCSV) {
+        if (!($GroupBySubscription)) {
+            throw "ERROR: Export to CSV file only works when 'GroupBySubscription' option is turned on"
+        }
+        if ($ExportFolder) {
+            $folderName = $ExportFolder+"\AttachedDisks"
+        } else {
+            $folderName = "AttachedDisks"
+        }
+        if (!(Test-Path -Path $folderName))
+        {
+            New-Item ($folderName) -ItemType Directory | Out-Null
+        }
+        foreach ($Sub in $AttachedDisks) {
+            $FileName = $folderName+"\Attached_("+$Sub.DiskCount+")"+$Sub.OwnerSubscription+".csv"
+            Get-AzsDisk -Status all -SharePath $MigrationSource -UserSubscriptionId $Sub.OwnerSubscription -Count 1000 | where {($_.DiskType -eq 'Disk') -and ($_.status -in @("OnlineMigration","Attached","Reserved"))} | Select `
+                Status, 
+                @{Name="DiskSubscription";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[1] }}, `
+                @{Name="DiskResourceGroup";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[2] }}, `
+                @{Name="DiskName";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[3] }}, `
+                UserResourceId,
+                ActualSizeGb,
+                ProvisionSizeGB | Export-Csv -Path $FileName
+        }
+    }
+    return $AttachedDisks
+}
+
+<#
+    .SYNOPSIS
+    Import disk migration candidates from CSV generated from tenant owner
+#>
+
+function ImportDiskMigrationCandidates {
+    param (
+        [parameter(Mandatory = $true, HelpMessage = "File path to the disk migration candidates CSV generated by tenant owner")]
+        [string]$CSVFilePath,
+
+        [parameter(Mandatory = $true, HelpMessage = "The migration type of disk, including 'Attached' and 'Unattached'")]
+        [ValidateSet('Attached','Unattached')]
+        [string]$MigrationType
+    )
+
+    $MigrationCandidateId = Import-Csv -Path $CSVFilePath
+    $MigrationCandidateDisk = @()
+    $FilterStatus = "all"
+    if ($MigrationType -eq "Unattached") {
+        $FilterStatus = "Recommended"
+    }
+    if ($MigrationCandidateId) {
+        $MigrationCandidateDisk = Get-AzsDisk -Status $FilterStatus -UserSubscriptionId $MigrationCandidateId[0].Subscription -Count 1000 | Where-Object {$_.UserResourceId -in $MigrationCandidateId.UserResourceId}
+    }
+    return $MigrationCandidateDisk
+}
---
+++ CapacityManagement/AzureStack.CapacityMgmtTenant.psm1
@@ -0,0 +1,227 @@
+﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# See LICENSE.txt in the project root for license information.
+
+<#
+    .SYNOPSIS
+    Import disks from CSV
+#>
+
+function GetDisksFromCSV {
+    param (
+        [parameter(Mandatory = $true, HelpMessage = "File path to the disk CSV")]
+        [string]$CSVFilePath
+    )
+    
+    if (!($CSVFilePath) -or !(Test-Path -Path $CSVFilePath)) {
+        throw "ERROR: CSV file doesn't exist. Please specify the correct file path of the disk CSV file"
+        return
+    } else {
+        $Disks = Import-Csv -Path $CSVFilePath
+    }
+    return $Disks
+}
+
+<#
+    .SYNOPSIS
+    Get snapshots linked to input disks
+#>
+
+function GetSnapshotsLinkToDisks {
+    param (
+        [parameter(Mandatory = $false, HelpMessage = "Import disks from CSV extracted by Cloud Operator")]
+        [switch] $ImportDiskCSV,
+
+        [parameter(Mandatory = $false, HelpMessage = "File path to the disk CSV")]
+        [string]$CSVFilePath
+    )
+
+    $Snapshots = @()
+    if ($ImportDiskCSV) {
+        $ImportDisks = GetDisksFromCSV -CSVFilePath $CSVFilePath
+        if ($ImportDisks) {
+            $Subscription = $ImportDisks[0].DiskSubscription
+            if ((Get-AzureRmContext).Subscription.Id -ne $Subscription) {
+                Select-AzureRmSubscription -Subscription $Subscription
+            }
+            $Snapshots = Get-AzureRmSnapshot | Where-Object {$_.CreationData.SourceResourceId -in $ImportDisks.UserResourceId}
+        }
+    } else {
+        $Snapshots = Get-AzureRmSnapshot
+    }
+    $Snapshots = $Snapshots  | select `
+        ResourceGroupName, `
+        @{Name="SnapshotName"; Expression={$_.Name}}, `
+        @{Name="SourceDiskName"; Expression={ $m = $_.CreationData.SourceResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/.*/(.*)"; $matches[3] }}, `
+        Id
+    return $Snapshots
+}
+
+<#
+    .SYNOPSIS
+    Get unattached disks
+#>
+
+function GetUnattachedDisks {
+    param (
+        [parameter(Mandatory = $false, HelpMessage = "Import disks from CSV extracted by Cloud Operator")]
+        [switch] $ImportDiskCSV,
+
+        [parameter(Mandatory = $false, HelpMessage = "File path to the disk CSV")]
+        [string]$CSVFilePath,
+
+        [parameter(Mandatory = $false, HelpMessage = "Query standalone unattached disks (which don't have related snapshots) as migration candidates and export to CSV")]
+        [switch]$MigrationCandidates,
+
+        [parameter(Mandatory = $false, HelpMessage = "File path to export migration candidates as CSV. If not specified, the candidates would be exported to '{subscription ID}_UnattachedMigrationCandidates.CSV' under current location")]
+        [string]$ExportFolder
+    )
+
+    $Disks = @()
+    if ($ImportDiskCSV) {
+        $ImportDisks = GetDisksFromCSV -CSVFilePath $CSVFilePath
+        if ($ImportDisks) {
+            $Subscription = $ImportDisks[0].DiskSubscription
+            if ((Get-AzureRmContext).Subscription.Id -ne $Subscription) {
+                Select-AzureRmSubscription -Subscription $Subscription
+            }
+            if ($MigrationCandidates) {
+                foreach ($disk in $ImportDisks) {
+                    $hasSnapshot = Get-AzureRmSnapshot | Where-Object {$_.CreationData.SourceResourceId -in $disk.UserResourceId}
+                    if (!($hasSnapshot)) {
+                        $Disks += $disk
+                    }
+                }
+                
+                $SubId = (Get-AzureRmContext).Subscription.Id
+                $FileName = "\"+$SubId+"_UnattachedMigrationCandidates.CSV"
+                if ($ExportFolder) {
+                    if (!(Test-Path -Path $ExportFolder)) {
+                        throw "ERROR: File path doesn't exist. Please specify the correct file path to export CSV file"
+                    } else {
+                        $FileName = $ExportFolder+$FileName
+                    }
+                }
+                $Disks | Export-Csv -Path $FileName
+            } else {
+                foreach ($disk in $ImportDisks) {
+                    $UnattachedDisk = Get-AzureRmDisk | Where-Object {$_.Id -eq $disk.UserResourceId}
+                    if ($UnattachedDisk) {
+                        $undisk = New-Object PsObject -Property @{ ResourceGroupName = $UnattachedDisk.ResourceGroupName ; DiskName = $UnattachedDisk.Name ; ActualSizeGb = $disk.ActualSizeGb ; ProvisionSizeGb = $disk.ProvisionSizeGb}
+                        $Disks += $undisk
+                    }
+                }
+            }
+        }
+    } else {
+        if ($MigrationCandidates) {
+            throw "ERROR: Query unattached standalond disks for migration only supports searching based on CSV exported by cloud operator. Please turn on the 'ImportDiskCSV' option and specify the CSV path"
+        }
+        $Disks = Get-AzureRmDisk | Where-Object {$_.ManagedBy -eq $null}
+    }
+    return $Disks
+}
+
+<#
+    .SYNOPSIS
+    Get attached disks
+#>
+
+function GetAttachedDisks {
+    param (
+        [parameter(Mandatory = $false, HelpMessage = "Import disks from CSV extracted by Cloud Operator")]
+        [switch] $ImportDiskCSV,
+
+        [parameter(Mandatory = $false, HelpMessage = "File path to the disk CSV")]
+        [string]$CSVFilePath,
+
+        [parameter(Mandatory = $false, HelpMessage = "Query standalone attached disks (which don't have related snapshots and were not created from image) which attached to deallocated VMs as migration candidates, and export to CSV")]
+        [switch]$MigrationCandidates,
+
+        [parameter(Mandatory = $false, HelpMessage = "File path to export migration candidates as CSV. If not specified, the candidates would be exported to '{subscription ID}_AttachedMigrationCandidates.CSV' under current location")]
+        [string]$ExportFolder,
+
+        [parameter(Mandatory = $false, HelpMessage = "Query owner VM of queried attached disks")]
+        [switch]$GroupByVM
+    )
+
+    $Disks = @()
+    $MigrationDisks = @()
+    if ($ImportDiskCSV) {
+        $ImportDisks = GetDisksFromCSV -CSVFilePath $CSVFilePath
+        if ($ImportDisks) {
+            $Subscription = $ImportDisks[0].DiskSubscription
+            if ((Get-AzureRmContext).Subscription.Id -ne $Subscription) {
+                Select-AzureRmSubscription -Subscription $Subscription
+            }
+            foreach ($disk in $ImportDisks) {
+                $hasSnapshot = Get-AzureRmSnapshot | Where-Object {$_.CreationData.SourceResourceId -in $disk.UserResourceId}
+                if (!($hasSnapshot)) {
+                    $QueryDisk = Get-AzureRmDisk | Where-Object {$_.Id -eq $disk.UserResourceId}
+                    $creationData = $QueryDisk.CreationData
+                    switch ($creationData.CreateOption)
+                    {
+                        "Empty"
+                        {
+                            $Disks += $QueryDisk
+                        }
+                        "Copy"
+                        {
+                            $SourceSnapshotExist = Get-AzureRmSnapshot | Where-Object {$_.Id -eq $creationData.SourceResourceId}
+                            if (!($snapshotExist)) {
+                                $Disks += $QueryDisk
+                            }
+                        }
+                    }
+                }
+            }
+            $AttachedVMs = $Disks | Group-Object -Property ManagedBy | select `
+                @{Name="VMSubscription";Expression={ $m = $_.Name -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)"; $matches[1] }}, `
+                @{Name="VMResourceGroup";Expression={ $m = $_.Name -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)"; $matches[2] }}, `
+                @{Name="VMName";Expression={ $m = $_.Name -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)/(.*)"; $matches[4] }}, `
+                @{Name="VMId"; Expression={$_.Name}}, `
+                @{Name="ImpactedDiskCount";Expression={ $_.Count }}
+            if ($MigrationCandidates) {
+                foreach ($VM in $AttachedVMs) {
+                    $CheckVm = Get-AzureRmVM -ResourceGroupName $VM.VMResourceGroup -Name $VM.VMName -Status
+                    if ($CheckVm) {
+                        if ("VM deallocated" -eq $CheckVm.Statuses.DisplayStatus[1]) {
+                            $MigrateTemp = ($Disks | Where-Object {$_.ManagedBy -eq $VM.VMId})
+                            $MigrationDisks += ($ImportDisks | Where-Object {$_.UserResourceId -in $MigrateTemp.Id})
+                        }
+                    } else {
+                        $MigrateTemp = ($Disks | Where-Object {$_.ManagedBy -eq $VM.VMId})
+                        $MigrationDisks += ($ImportDisks | Where-Object {$_.UserResourceId -in $MigrateTemp.Id})
+                    }
+                }
+                $Disks = $MigrationDisks
+                
+                $SubId = (Get-AzureRmContext).Subscription.Id
+                $FileName = "\"+$SubId+"_AttachedMigrationCandidates.CSV"
+                if ($ExportFolder) {
+                    if (!(Test-Path -Path $ExportFolder)) {
+                        throw "ERROR: File path doesn't exist. Please specify the correct file path to export CSV file"
+                    } else {
+                        $FileName = $ExportFolder+$FileName
+                    }
+                }
+                $Disks | Export-Csv -Path $FileName
+            }
+        }
+    } else {
+        if ($MigrationCandidates) {
+            throw "ERROR: Query attached standalond disks for migration only supports searching based on CSV exported by cloud operator. Please turn on the 'ImportDiskCSV' option and specify the CSV path"
+        }
+        $Disks = Get-AzureRmDisk | Where-Object {$_.ManagedBy -ne $null}
+        $AttachedVMs = $Disks | Group-Object -Property ManagedBy | select `
+            @{Name="VMSubscription";Expression={ $m = $_.Name -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)"; $matches[1] }}, `
+            @{Name="VMResourceGroup";Expression={ $m = $_.Name -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)"; $matches[2] }}, `
+            @{Name="VMName";Expression={ $m = $_.Name -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/(.*)/(.*)"; $matches[4] }}, `
+            @{Name="VMId"; Expression={$_.Name}}, `
+            @{Name="ImpactedDiskCount";Expression={ $_.Count }}
+    }
+    if ($GroupByVM) {
+        return $AttachedVMs
+    } else {
+        return $Disks
+    }
+}
